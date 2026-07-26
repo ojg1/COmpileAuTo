947ab7d8d29ef96cc98b20feafc1819f5355df73
OrangeJuiceGuy1 <ericlin1253@gmail.com>
2026-07-25 23:17:02 -0400

    0.1.1

--- compauto.cpp
+++ compauto.cpp
@@ -4,6 +4,8 @@
 #include <ranges>
 #include <cstdlib>
 #include <filesystem>
+#include <regex>
+
 
 /*
 
@@ -28,25 +30,36 @@ using std::getline;
 using std::string;
 using std::vector;
 using std::unordered_map;
+using std::regex;
+using std::smatch;
 
 struct keywTypes {
     string Alias;
-    string PathName;
+    string FileAlias;
     string Version;
+    string Path;
     string ProgramLangSupport;
     bool Found;
 };
 
-void ParsePathCompiler(string os, vector<string> &comps) {
+void ParsePathCompiler(string os,  unordered_map<string,keywTypes> &comps) {
 
     if (os == "Windows") {
 
-        unordered_map<string,keywTypes> keyw = {
-            {"gcc",   {"GNU Compiler Collection",           "gcc.exe",      "N/A", "C, Universal",                                   false}},
-            {"g++",   {"GNU C++ Compiler",                  "g++.exe",      "N/A", "C++",                                            false}},
-            {"clang", {"C Language Family Frontend",        "clang.exe",    "N/A", "Variants of C, C++ (including objective-c<++>)", false}},
-            {"msvc",  {"Microsoft Visual C++ Compiler",     "cl.exe",       "N/A", "C++",                                            false}},
-            {"nvcc",  {"NVIDIA Cuda Compiler",              "nvcc.exe",     "N/A", "C++/CUDA",                                       false}}
+        unordered_map<string,keywTypes> keyc = {
+            {"gcc.exe",   {"GNU Compiler Collection",           "gcc",      "N/A", "N/A", "C, Universal",                                   false}},
+            {"g++.exe",   {"GNU C++ Compiler",                  "g++",      "N/A", "N/A", "C++",                                            false}},
+            {"clang.exe", {"C Language Family Frontend",        "clang",    "N/A", "N/A", "Variants of C, C++ (including objective-c<++>)", false}},
+            {"msvc.exe",  {"Microsoft Visual C++ Compiler",     "cl",       "N/A", "N/A", "C++",                                            false}},
+            {"nvcc.exe",  {"NVIDIA Cuda Compiler",              "nvcc",     "N/A", "N/A", "C++/CUDA",                                       false}}
+        };
+
+        vector<string> ekeyc = {
+            "gcc.exe",
+            "g++.exe",
+            "clang.exe",
+            "cl.exe",
+            "nvcc.exe"
         };
 
 
@@ -60,13 +73,39 @@ void ParsePathCompiler(string os, vector<string> &comps) {
             if (std::filesystem::exists(dir) && std::filesystem::is_directory(dir)) {
                 for (const auto &entry : std::filesystem::directory_iterator(dir)) {
 
-                    if () {
+                    string filename = entry.path().filename().string();
+
+                    if (std::find(ekeyc.begin(), ekeyc.end(), filename) == ekeyc.end()) {
+
+                        keyc[filename].FileAlias = filename;
+                        keyc[filename].Path = entry.path().parent_path().string() + filename;
+                        keyc[filename].Found = true;
+
+                        string pathComm = keyc[filename].Path + "--version";
+
+                        string result = "";
+                        char buffer[128];
+
+                        FILE* output = _popen(pathComm.c_str(), "r");
+
+                        while (fgets(buffer, sizeof(buffer), output) != nullptr) {
+                            result += buffer;
+                        };
+
+                        regex p("\d+\.\d+(\.\d+)*");
+                        smatch match;
+
+                        if (std::regex_match(result, match, p)) {
+                            keyc[filename].Version = match[0];
+                        };
 
                     };
 
                 };
             };
         };
+
+        comps = keyc;
 
     } else if (os == "Linux") {
 
@@ -85,10 +124,13 @@ int main(int argc, char *argv[]){
         args.push_back(argv[i]);
     };  
 
-    vector<string> comps = {};
+    unordered_map<string,keywTypes> comps = {};
     string SelectedLanguage = "detectFileExtension";
+    string SearchCompilerMethod = "fullFilename";
     string CurrentCompiler = "";
     string OperatingSystem = "";
+
+    string RunString = "";
 
     if (args[0] != "compauto" || args[0] != "coat") {return 0;}
 
@@ -105,9 +147,19 @@ int main(int argc, char *argv[]){
         cout << "These compilers were found in your PATH environment variable. \n";
         cout << "Select compiler: \n";
 
-        string path = std::getenv("PATH");
+        vector<string> available = {};
+
+        int i = 0;
+
+        for (const auto& [key,value] : comps) {
+            if (value.Found) {
+                available.push_back(key);
+                cout >> "[" + std::to_string(i) + "] " + value.FileAlias + " (" + value.Alias + ")";
+            };
+        };
 
 
+        
     };
 
     return 0;
